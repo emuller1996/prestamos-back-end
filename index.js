64ebae6f79ce34fc18cdcd58fcdd1755598fabe1
emuller1996 <stefano.muller@hotmail.com>
2023-11-22 23:17:11 -0500

    change postgres

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import listen from "./src/app.js";
 
-listen.listen(3001, () => {
-  console.log(`Server listening at port ${3001}`); // eslint-disable-line no-console
+listen.listen(process.env.PORT, () => {
+  console.log(`Server listening at port ${process.env.PORT}`); // eslint-disable-line no-console
 });
